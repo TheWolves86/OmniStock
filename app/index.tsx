6594f77bf25fce56c23597cceb12ec13d01d5969
TheWolves86 <thewolves86.dev@gmail.com>
2026-06-03 16:11:27 +0600

    Some changes

---
+++ app/index.tsx
@@ -0,0 +1,10 @@
+import { Redirect } from 'expo-router'
+import React from 'react'
+
+const Appreturn = () => {
+  return (
+    <Redirect href="@/onboarding/screen1" />
+  )
+}
+
+export default Appreturn
---
+++ app/onboarding/screen1.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View, Text, StyleSheet } from 'react-native'
+
+const screen1 = () => {
+  return (
+    <View className="flex-">
+      <Text>screen1</Text>
+    </View>
+  )
+}
+
+export default screen1
