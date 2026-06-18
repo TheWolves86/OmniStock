f239e7be4933b083b102d79d33934149f41ddd8d
TheWolves86 <thewolves86.dev@gmail.com>
2026-06-18 23:54:03 +0600

    Add product feature added on inventory page

--- app/(tabs)/inventory.tsx
+++ app/(tabs)/inventory.tsx
@@ -1,35 +1,42 @@
-import { View, ScrollView} from "react-native";
+import { View, ScrollView, Pressable, Text} from "react-native";
 import ProductCard from "../../components/productcard";
 import React, {useState, useEffect} from "react"
-import { getProducts } from "../../lib/productService"; 
+import { getProducts } from "../../lib/productService";
+import { useRouter } from 'expo-router'
 
 export default function Inventory() {
+  const router = useRouter();
   const [products, setProducts] = useState<any[]>([]);
   useEffect(() => {
-  const data = getProducts();
+    const data = getProducts();
 
-  setProducts(data as any[]);
-}, []);
+    setProducts(data as any[]);
+  }, []);
   return (
-    <ScrollView
-    contentContainerStyle={{
-      padding: 16,
-   }}
-  >
-    {products.map((product) => (
-      <ProductCard
-        key={product.id}
-        name={product.name}
-        sku={product.sku}
-        category={product.category}
-        purchasePrice={product.purchasePrice}
-        price={product.sellingPrice}
-        gstRate={product.gstRate}
-        stock={product.stock}
-        lowStockThreshold={product.lowStockThreshold}
-        createdAt={product.createdAt}
-      />
-    ))}
-  </ScrollView>
+    <View style={{flex: 1}}>
+      <ScrollView
+      contentContainerStyle={{
+        padding: 16,
+    }}
+    >
+      {products.map((product) => (
+        <ProductCard
+          key={product.id}
+          name={product.name}
+          sku={product.sku}
+          category={product.category}
+          purchasePrice={product.purchasePrice}
+          price={product.sellingPrice}
+          gstRate={product.gstRate}
+          stock={product.stock}
+          lowStockThreshold={product.lowStockThreshold}
+          createdAt={product.createdAt}
+        />
+      ))}
+    </ScrollView>
+    <Pressable style={{position: 'absolute',bottom:10,right:20, width:56,height:56,borderRadius: 28,backgroundColor: "#008080", justifyContent: "center", alignItems: "center", elevation: 8}} onPress={() => router.push('../onboarding/addproduct')}>
+      <Text style={{color: "#FFFFFF",fontSize: 28, fontWeight: "300"}}>+</Text>
+    </Pressable>
+  </View>
   );
 }
